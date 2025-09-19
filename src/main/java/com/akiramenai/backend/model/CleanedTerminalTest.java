4fa8d8b65050d7c89396898409fb542100d4039c
Twaha Rahman <mahi6703890@gmail.com>
2025-09-19 16:35:20 +0600

    Add the description field in the CleanedTerminalTest

--- src/main/java/com/akiramenai/backend/model/CleanedTerminalTest.java
+++ src/main/java/com/akiramenai/backend/model/CleanedTerminalTest.java
@@ -3,13 +3,15 @@ package com.akiramenai.backend.model;
 public record CleanedTerminalTest(
     String itemId,
     String courseId,
-    String question
+    String question,
+    String description
 ) {
   public CleanedTerminalTest(TerminalTest terminalTest) {
     this(
         terminalTest.getItemId(),
         terminalTest.getCourseId().toString(),
-        terminalTest.getQuestion()
+        terminalTest.getQuestion(),
+        terminalTest.getDescription()
     );
   }
 }
