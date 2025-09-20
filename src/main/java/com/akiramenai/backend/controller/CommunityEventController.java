cb873610bdbb23b982af7c018d2895d985d6aabb
Twaha Rahman <mahi6703890@gmail.com>
2025-09-20 16:22:50 +0600

    fix http verb

--- src/main/java/com/akiramenai/backend/controller/CommunityEventController.java
+++ src/main/java/com/akiramenai/backend/controller/CommunityEventController.java
@@ -10,6 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -61,7 +62,7 @@ public class CommunityEventController {
   }
 
 
-  @GetMapping("api/public/add/events")
+  @PostMapping("api/public/add/events")
   public void addCommunityEvents(
       HttpServletRequest httpRequest,
       HttpServletResponse httpResponse,
